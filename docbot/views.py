bd34b7ecc841b551b48067aa682bc3e35dd5c076
Xufeng Bai <baix0531@gmail.com>
2023-01-02 11:57:55 +0000

    Created a simple chatbot. Able to reply a single message.

--- docbot/views.py
+++ docbot/views.py
@@ -1,3 +1,17 @@
+import time
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST['message']
+        messages = request.session.get('messages', [])
+        messages.append(message)
+        # Add a delay before the chatbot responds
+        time.sleep(1)
+        response = "This is the chatbot's response."
+        messages.append(response)
+        request.session['messages'] = messages
+    else:
+        messages = request.session.get('messages', [])
+    return render(request, 'chatbot.html', {'messages': messages})
--- system/asgi.py
+++ system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DocBot project.
+ASGI config for docbot project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DocBot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'system.settings')
 
 application = get_asgi_application()
