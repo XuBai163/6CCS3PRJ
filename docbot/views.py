80f1351bbd84b85ea2fab9458c64087c6a43dbaa
Xufeng Bai <baix0531@gmail.com>
2023-02-25 12:09:55 +0000

    Add machine learning models development process.

--- docbot/views.py
+++ docbot/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# from .chatbot_engine import process_message
+from docbot.chatbot_engine import message_processor
 
 def home(request):
     return render(request, 'home.html')
@@ -12,3 +12,19 @@ def getResponse(request):
     text = request.GET.get('text')
     response_text = "Hello World, your input text is: " + text
     return JsonResponse({'text': response_text, 'user': False, 'chatbot': True})
+
+# from django.views.generic.edit import FormView
+# from django.urls import reverse_lazy
+# from .forms import ChatbotForm
+# from .machine_learning import predict_disease
+
+# class ChatbotView(FormView):
+#     template_name = 'chatbot.html'
+#     form_class = ChatbotForm
+#     success_url = reverse_lazy('chatbot')
+
+#     def form_valid(self, form):
+#         user_input = form.cleaned_data.get('input')
+#         prediction = predict_disease(user_input)
+#         self.extra_context = {'message': f'You may have {prediction}.'}
+#         return super().form_valid(form)
--- system/urls.py
+++ system/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
 from docbot import views
@@ -22,4 +23,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('chatbot/', views.chatbot, name="chatbot"),
     path('getResponse/', views.getResponse, name="getResponse"),
+    path('api-auth/', include('rest_framework.urls')),
 ]
