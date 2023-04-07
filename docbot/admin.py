b5df5ca23e3c42f9ed7ec68bfaab77bf4f95ab75
Xufeng Bai <baix0531@gmail.com>
2023-04-07 14:42:52 +0300

    Add admin.

--- docbot/admin.py
+++ docbot/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Message, User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for users."""
+
+    list_display = [
+        'username', 'first_name', 'last_name', 'email',
+    ]
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for posts."""
+
+    list_display = [
+        'user', 'text', 'is_user_message',
+    ]
